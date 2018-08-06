c2e48a37c00e1c84161f040334583b0aeb8b3d62
zzgfw <34565461+zzgfw@users.noreply.github.com>
2018-08-06 15:53:24 +0800

    Update new-time.py
    加入两个注释
    更改变量名

--- new-time.py
+++ new-time.py
@@ -7,16 +7,18 @@ header = {
 url = input('Enter the url: ')
 page = requests.get(url, headers = header).content
 
+#保存到本地
 def save_img(img_addr):
     response = requests.get(img_addr, headers = header).content
     name = img_addr[-12:-4]
     with open(name + '.png', 'wb') as file:
         file.write(response)
-        
+
+#解析主页面
 soup = BeautifulSoup(page, 'html.parser')
 linkPool = soup.find_all('td', class_='tal')
 for links in linkPool:
     link = links.find('h3')
-    line1 = link.get_text()
-    line2 = 'http://aa.dety.men/' + link.a.get('href')
-    print(line1, '======', line2)
+    title = link.get_text()
+    address = 'http://aa.dety.men/' + link.a.get('href')
+    print(title, '======', address)
